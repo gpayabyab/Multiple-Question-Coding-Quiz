f578e4e779630a6c65e14c98f7bf93d1ed061cd6
Gabrielle 🌿 <gabrielle@GabrielesLaptop.fios-router.home>
2023-12-11 17:24:15 -0500

    add pseudo code to html and js

---
+++ assests/js/script.js
@@ -0,0 +1,17 @@
+//I click the start button 
+
+//the timer starts 
+
+//I am presented with a question 
+
+//I answer a question
+
+//I am presented with another question
+
+// I answer a question incorrectly 
+
+//time is subtracted from the clock 
+
+//WHEN all questions are answered or the timer reaches 0 THEN the game is over 
+
+//I can save my initials and score 
