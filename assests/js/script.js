cc2e3e440837b136ef3a493002a81f24a2d8d8a0
Gabrielle 🌿 <gabrielle@GabrielesLaptop.fios-router.home>
2023-12-11 23:02:19 -0500

    create start button, first question and attempted to add shuffle questions' ls

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,17 +1,28 @@
-//I click the start button 
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
-//the timer starts 
+const shuffledQuestions, currentQuestionsIndex 
 
-//I am presented with a question 
+startButton.addEventListener('click', startGame)
 
-//I answer a question
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random()- .5)
+    currentQuestionIndex - 0
+    questionContainerElement.classList.remove('hide')
+    seeNextQuestion()
+}
+// start button is working however, not sure why there's an uncaught syntac error for the first line 
 
-//I am presented with another question
+function setNextQuestion 
 
-// I answer a question incorrectly 
-
-//time is subtracted from the clock 
-
-//WHEN all questions are answered or the timer reaches 0 THEN the game is over 
-
-//I can save my initials and score 
+const question = [
+    question: 'What is HTML',
+    answers: [
+        { text: 'standard markup language for creating Web pages', correct: false},
+        { text: 'Hypertext Markup Language', correct: false},
+        { text: 'Describes the structure of a web page', correct: false},
+        { text: 'All of the above', correct: true}
+    ]
+]
